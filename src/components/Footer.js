b70ef5883be57b4f48c22beb7e0fc12ce2827364
Ban Hun <banhun@Banui-MacBookPro.local>
2024-07-03 11:12:12 +0900

    정보 및 이메일 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const Footer = () => (
-  <footer>
-    <hr></hr>
-    <p>footer</p>
-  </footer>
-);
+const Footer = () => {
+  const adminEmail = "iteasy.ops.dev@gmail.com";
+  const adminName = "ITEASY Service Ops Center";
+
+  return (
+    <footer>
+      <hr />
+      <p>Contact Information:</p>
+      <p>{adminName} - <a href={`mailto:${adminEmail}`}>{adminEmail}</a></p>
+    </footer>
+  );
+};
 
 export default Footer;
