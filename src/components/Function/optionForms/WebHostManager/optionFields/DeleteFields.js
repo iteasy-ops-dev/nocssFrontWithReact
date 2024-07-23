b1509e05ca7f7fd9e45690ff55002ad4d6715876
Ban Hun <banhun@Banui-MacBookPro.local>
2024-07-23 15:11:30 +0900

    webhost manager 작업

--- src/components/Function/optionForms/WebHostManager/optionFields/DeleteFields.js
+++ src/components/Function/optionForms/WebHostManager/optionFields/DeleteFields.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const DeleteFields = ({ formData, onChange }) => {
+  // State to manage whether to show the database options
+  const [showDbOptions, setShowDbOptions] = useState(false);
+
+  // Handler for the checkbox to toggle the database options
+  const handleDbOptionsToggle = () => {
+    setShowDbOptions(!showDbOptions);
+  };
+
   return (
     <div>
       <div>
@@ -8,49 +16,75 @@ const DeleteFields = ({ formData, onChange }) => {
           삭제할 계정:
           <input
             type="text"
-            name="options.delete_user_id"
-            value={formData.options.delete_user_id}
+            name="options.user_id"
+            value={formData.options.user_id}
             onChange={onChange}
-            placeholder="delete_user"
-          />
-        </label>
-      </div>
-			<div>
-        <label>
-          db admin:
-          <input
-            type="text"
-            name="options.mysql_root_user"
-            value={formData.options.mysql_root_user}
-            onChange={onChange}
-            placeholder="root"
+            placeholder="iteasy"
           />
         </label>
       </div>
       <div>
         <label>
-          db admin 비밀번호:
+          DB옵션 보기:
           <input
-            type="text"
-            name="options.mysql_root_password"
-            value={formData.options.mysql_root_password}
-            onChange={onChange}
-            placeholder="mysql_root_password"
+            type="checkbox"
+            checked={showDbOptions}
+            onChange={handleDbOptionsToggle}
           />
         </label>
       </div>
-      <div>
-        <label>
-          삭제할 db 이름:
-          <input
-            type="text"
-            name="options.delete_db_name"
-            value={formData.options.delete_db_name}
-            onChange={onChange}
-            placeholder="delete_db_name"
-          />
-        </label>
-      </div>
+      {showDbOptions && (
+        <>
+          <div>
+            <label>
+              db admin:
+              <input
+                type="text"
+                name="options.mysql_root_user"
+                value={formData.options.mysql_root_user}
+                onChange={onChange}
+                placeholder="root"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              db admin 비밀번호:
+              <input
+                type="text"
+                name="options.mysql_root_password"
+                value={formData.options.mysql_root_password}
+                onChange={onChange}
+                placeholder="mysql_root_password"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              db 계정:
+              <input
+                type="text"
+                name="options.db_user"
+                value={formData.options.db_user}
+                onChange={onChange}
+                placeholder="db_user"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              db 이름:
+              <input
+                type="text"
+                name="options.db_name"
+                value={formData.options.db_name}
+                onChange={onChange}
+                placeholder="db_name"
+              />
+            </label>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/Function/optionForms/WebHostManager/optionFields/SetupFields.js
+++ src/components/Function/optionForms/WebHostManager/optionFields/SetupFields.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SetupFields = ({ formData, onChange }) => {
+  // State to manage whether to show the database options
+  const [showDbOptions, setShowDbOptions] = useState(false);
+
+  // Handler for the checkbox to toggle the database options
+  const handleDbOptionsToggle = () => {
+    setShowDbOptions(!showDbOptions);
+  };
+
   return (
     <div>
       <div>
@@ -24,66 +32,6 @@ const SetupFields = ({ formData, onChange }) => {
             value={formData.options.user_pass}
             onChange={onChange}
             placeholder="iteasy"
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          db admin:
-          <input
-            type="text"
-            name="options.mysql_root_user"
-            value={formData.options.mysql_root_user}
-            onChange={onChange}
-            placeholder="root"
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          db admin 비밀번호:
-          <input
-            type="text"
-            name="options.mysql_root_password"
-            value={formData.options.mysql_root_password}
-            onChange={onChange}
-            placeholder="mysql_root_password"
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          db 계정:
-          <input
-            type="text"
-            name="options.db_user"
-            value={formData.options.db_user}
-            onChange={onChange}
-            placeholder="db_user"
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          db 계정 비밀번호:
-          <input
-            type="text"
-            name="options.db_password"
-            value={formData.options.db_password}
-            onChange={onChange}
-            placeholder="db_password"
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          db 이름:
-          <input
-            type="text"
-            name="options.db_name"
-            value={formData.options.db_name}
-            onChange={onChange}
-            placeholder="db_name"
           />
         </label>
       </div>
@@ -123,8 +71,83 @@ const SetupFields = ({ formData, onChange }) => {
           />
         </label>
       </div>
+      <div>
+        <label>
+          DB옵션 보기:
+          <input
+            type="checkbox"
+            checked={showDbOptions}
+            onChange={handleDbOptionsToggle}
+          />
+        </label>
+      </div>
+      {showDbOptions && (
+        <>
+          <div>
+            <label>
+              db admin:
+              <input
+                type="text"
+                name="options.mysql_root_user"
+                value={formData.options.mysql_root_user}
+                onChange={onChange}
+                placeholder="root"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              db admin 비밀번호:
+              <input
+                type="text"
+                name="options.mysql_root_password"
+                value={formData.options.mysql_root_password}
+                onChange={onChange}
+                placeholder="mysql_root_password"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              db 계정:
+              <input
+                type="text"
+                name="options.db_user"
+                value={formData.options.db_user}
+                onChange={onChange}
+                placeholder="db_user"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              db 계정 비밀번호:
+              <input
+                type="text"
+                name="options.db_password"
+                value={formData.options.db_password}
+                onChange={onChange}
+                placeholder="db_password"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              db 이름:
+              <input
+                type="text"
+                name="options.db_name"
+                value={formData.options.db_name}
+                onChange={onChange}
+                placeholder="db_name"
+              />
+            </label>
+          </div>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default SetupFields;
+
